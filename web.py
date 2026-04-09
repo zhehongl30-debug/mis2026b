4e6f89e13e9eb5786a9d9581139f696516f2603b
zhehongl30-debug <zhehongl30@gmail.com>
2026-04-09 11:32:55 +0800

    0409B

--- web.py
+++ web.py
@@ -1,5 +1,24 @@
 from flask import Flask, render_template,request
+
 from datetime import datetime
+
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# 判斷是在 Vercel 還是本地
+if os.path.exists('serviceAccountKey.json'):
+    # 本地環境：讀取檔案
+    cred = credentials.Certificate('serviceAccountKey.json')
+else:
+    # 雲端環境：從環境變數讀取 JSON 字串
+    firebase_config = os.getenv('FIREBASE_CONFIG')
+    cred_dict = json.loads(firebase_config)
+    cred = credentials.Certificate(cred_dict)
+
+firebase_admin.initialize_app(cred)
+
 import math
 app = Flask(__name__)
 
@@ -12,7 +31,19 @@ def index():
     link += "<a href = /welcome?u=zhehong&d=靜宜大學資管系>get</a><hr>"
     link += "<a href = /account>POST</a><hr>"
     link += "<a href='/calculate'>次方與根號計算</a><hr>"
+    link += "<a href=/read>讀取Firestore資料</a><hr>"
     return link
+
+@app.route("/read")
+def read():
+    Result = ""
+    db = firestore.client()
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
 
 @app.route("/mis")
 def course():
