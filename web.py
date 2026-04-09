804ff5f5eb8b3a4b8236eee10ddb7649131ca9a9
zhehongl30-debug <zhehongl30@gmail.com>
2026-04-09 11:04:18 +0800

    0409

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    link = "<h1>歡迎進入徐晣紘的網站</h1>"
+    link = "<h1>歡迎進入徐晣紘的網站20260409</h1>"
     link += "<a href = /mis>課程</a><hr>"
     link += "<a href = /today>現在日期時間</a><hr>"
     link += "<a href = /me>關於我</a><hr>"
