2652d8bd51c837b59b9b30153a037aac1a013366
zhehongl30-debug <zhehongl30@gmail.com>
2026-05-28 12:24:06 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -108,28 +108,7 @@ def webhook():
         info += result
 
     elif (action == "input.unknown"):
-        #info =req["queryResult"]["queryText"] 
-        instruction_text = (
-        "你是一個熱心且知識豐富的專業智慧助理。"
-        "對於使用者的提問，請回覆重點的關鍵字，不要重述問題。"         
-        )
-
-        ai_config = types.GenerateContentConfig(
-            max_output_tokens=500, 
-            system_instruction=instruction_text
-        )
-        
-        response = client.models.generate_content(
-            model='gemini-3.5-flash-lite', 
-            contents=req["queryResult"]["queryText"],
-            config=ai_config,
-        )
-
-        if response.text:
-            info = response.text
-        else:
-            info = "抱歉，我現在無法生成回應，請稍後再試。"
-
+        info =req["queryResult"]["queryText"] 
 
     return make_response(jsonify({"fulfillmentText": info}))
 
